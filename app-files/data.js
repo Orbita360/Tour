7cdd038c3c601565ac5d00daa9eeaf8bf34a7f91
Orbita360 <orbitagencia@gmail.com>
2017-04-02 23:22:02 -0300

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360_0006_stitch_xhc",
+      "name": "360_0006_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 0.20297326819512485,
+        "pitch": -0.2579372718323416,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.988291464337438,
+          "pitch": 0.26066054722188703,
+          "rotation": 0,
+          "target": "1-360_0017_stitch_xhc"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360_0017_stitch_xhc",
+      "name": "360_0017_Stitch_XHC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8478136423080773,
+          "pitch": -0.048179285238585834,
+          "rotation": 0,
+          "target": "0-360_0006_stitch_xhc"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
